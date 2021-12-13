9ed69bc7ddba636d544c6da5f6f379b6e4e7bed6
John Omotani <john.omotani@ukaea.uk>
2021-12-13 19:53:07 +0000

    Use Cray-recommended optimisation flags for optimised build
    Note, do not compile PETSc with `-fast` - trying to use that flag results in
    configuration failure, with the error
    ```
    Unknown Fortran name mangling: Are you sure the C and Fortran compilers are compatible?
      Perhaps one is 64 bit and one is 32 bit?
    ```

--- archer/build-dependencies.sh
+++ archer/build-dependencies.sh
@@ -22,9 +22,7 @@ mkdir petsc-build
 wget https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.16.1.tar.gz
 tar xzf petsc-3.16.1.tar.gz
 cd petsc-3.16.1
-#./configure --CC=cc --CXX=CC --FC=ftn COPTFLAGS="-O3" CXXOPTFLAGS="-O3" FOPTFLAGS="-O3" --with-batch --known-64-bit-blas-indices=0 --known-sdor-returns-double=0 --known-snrm2-returns-double=0 --download-hypre --with-debugging=0 --prefix=../petsc-build
-./configure --CC=cc --CXX=CC --FC=ftn COPTFLAGS="-O3" CXXOPTFLAGS="-O3" FOPTFLAGS="-O3" --with-batch --known-64-bit-blas-indices=0 --known-sdor-returns-double=0 --known-snrm2-returns-double=0 --with-fortran-bindings=0 --download-hypre --with-debugging=0 --prefix=../petsc-build
-#./configure --CC=cc --CXX=CC COPTFLAGS="-O3" CXXOPTFLAGS="-O3" --with-batch --known-64-bit-blas-indices=0 --known-sdor-returns-double=0 --known-snrm2-returns-double=0 --with-fortran-bindings=0 --download-hypre --with-debugging=0 --prefix=../petsc-build
+./configure --CC=cc --CXX=CC --FC=ftn COPTFLAGS="-Ofast" CXXOPTFLAGS="-Ofast" FOPTFLAGS="-Ofast" --with-batch --known-64-bit-blas-indices=0 --known-sdor-returns-double=0 --known-snrm2-returns-double=0 --with-fortran-bindings=0 --download-hypre --with-debugging=0 --prefix=../petsc-build
 make -j 16 PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-opt all
 make -j 16 PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-opt install
 make -j 16 PETSC_DIR=$PWD/../petsc-build PETSC_ARCH="" check
