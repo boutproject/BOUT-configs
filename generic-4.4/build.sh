b13030d0354ab93aaad42b6b81e3aab7f6f11e53
John Omotani <john.omotani@ukaea.uk>
2022-02-02 21:04:01 +0000

    Allow BOUT++ to configure/compile alongside an active conda environment
    Set `-DBOUT_IGNORE_CONDA_ENV=ON` so that BOUT++'s CMake configuration
    ignores everything installed by conda. This seems to be enough to
    prevent library conflicts.

--- generic-4.4/build.sh
+++ generic-4.4/build.sh
@@ -7,5 +7,5 @@ cd BOUT-dev
 
 git submodule update --init --recursive
 
-PETSC_DIR=$PWD/../dependencies/petsc-build PETSC_ARCH="" cmake . -B build -DCMAKE_BUILD_TYPE=Release -DCHECK=0 -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON
+cmake . -B build -DCMAKE_BUILD_TYPE=Release -DCHECK=0 -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON -DPETSC_DIR=$PWD/../dependencies/petsc-build -DPETSC_ARCH="" -DBOUT_IGNORE_CONDA_ENV=ON
 cmake --build build -j 4
