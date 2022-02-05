2be6d2cc0ab109c104a46197b8846f6849174d41
John Omotani <john.omotani@ukaea.uk>
2022-02-05 19:42:45 +0100

    Add gnu to modules for intel2018 compilers
    Allows icpc to pick up a more recent version of libstdc++, which
    prevents compilation errors.

--- marconi-intel2018/scripts/build-petsc.sh
+++ marconi-intel2018/scripts/build-petsc.sh
@@ -16,7 +16,7 @@ mkdir petsc-build
 wget https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.16.3.tar.gz
 tar xzf petsc-3.16.3.tar.gz
 cd petsc-3.16.3
-./configure COPTFLAGS="-O3" CXXOPTFLAGS="-O3" FOPTFLAGS="-O3" --download-hypre --with-hdf5=0 --with-debugging=0 --prefix=../petsc-build
+CC=icc CXX=icpc MPICC=mpiicc MPICXX=mpiicpc ./configure COPTFLAGS="-O3" CXXOPTFLAGS="-O3" FOPTFLAGS="-O3" --download-hypre --with-hdf5=0 --with-debugging=0 --prefix=../petsc-build
 make -j 16 PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-opt all
 make -j 16 PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-opt install
 make -j 16 PETSC_DIR=$PWD/../petsc-build PETSC_ARCH="" check
