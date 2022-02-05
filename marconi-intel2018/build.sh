867f1aec6ccfd9c5315546d2de23e539d29bbb43
John Omotani <john.omotani@ukaea.uk>
2022-02-05 16:04:11 +0100

    Don't pass `-std=c++14` in CMAKE_CXX_FLAGS_RELEASE
    Does not take effect from there, need to edit CMakeLists.txt in BOUT-dev
    itself...

--- marconi-intel2018/build.sh
+++ marconi-intel2018/build.sh
@@ -9,6 +9,6 @@ cd ../BOUT-dev
 
 git submodule update --init --recursive
 
-cmake . -B build-intel2018 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-std=c++11 -O3 -axCORE-AVX512 -march=skylake-avx512 -mtune=skylake-avx512" -DCHECK=0 -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON -DPETSC_DIR=$DEPS_PATH/petsc-build -DPETSC_ARCH="" -DBOUT_USE_HDF5=OFF -DBOUT_IGNORE_CONDA_ENV=ON
+cmake . -B build-intel2018 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3 -axCORE-AVX512 -march=skylake-avx512 -mtune=skylake-avx512" -DCHECK=0 -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON -DPETSC_DIR=$DEPS_PATH/petsc-build -DPETSC_ARCH="" -DBOUT_USE_HDF5=OFF -DBOUT_IGNORE_CONDA_ENV=ON
 
 cmake --build build-intel2018 -j 16
