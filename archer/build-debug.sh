8dc2090341d3258b0b0035789c33840f11610460
John Omotani <john.omotani@ukaea.uk>
2022-03-04 11:40:00 +0000

    Add `-DBOUT_IGNORE_CONDA_ENV=ON` for compatibility with conda
    If conda is active, it can lead to link errors.
    `-DBOUT_IGNORE_CONDA_ENV=ON` avoids these by telling BOUT++ to ignore
    all paths in the conda environment.

--- archer/build-debug.sh
+++ archer/build-debug.sh
@@ -7,5 +7,5 @@ cd BOUT-dev
 
 git submodule update --init --recursive
 
-PETSC_DIR=$PWD/../dependencies/petsc-build PETSC_ARCH="" cmake . -B build-debug -DCMAKE_BUILD_TYPE=Debug -DCHECK=4 -DCMAKE_CXX_COMPILER=CC -DCMAKE_FTN_COMPILER=ftn -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON
+PETSC_DIR=$PWD/../dependencies/petsc-build PETSC_ARCH="" cmake . -B build-debug -DCMAKE_BUILD_TYPE=Debug -DCHECK=4 -DCMAKE_CXX_COMPILER=CC -DCMAKE_FTN_COMPILER=ftn -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON -DBOUT_IGNORE_CONDA_ENV=ON
 cmake --build build-debug -j 16
