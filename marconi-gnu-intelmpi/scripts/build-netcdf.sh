dd73fc9987acb21cb993538f14910bdb49ad7411
John Omotani <john.omotani@ukaea.uk>
2022-03-04 09:32:41 +0100

    Fix netcdf-c download link
    Link on the unidata.ucar.edu site was changed.

--- marconi-gnu-intelmpi/scripts/build-netcdf.sh
+++ marconi-gnu-intelmpi/scripts/build-netcdf.sh
@@ -6,7 +6,7 @@ set -e
 cd dependencies
 DEPS_ROOT=$(pwd)
 mkdir netcdf-build
-wget https://downloads.unidata.ucar.edu/netcdf-c/4.8.1/src/netcdf-c-4.8.1.tar.gz
+wget https://downloads.unidata.ucar.edu/netcdf-c/4.8.1/netcdf-c-4.8.1.tar.gz
 tar xzf netcdf-c-4.8.1.tar.gz
 cd netcdf-c-4.8.1
 CPPFLAGS="-I$DEPS_ROOT/hdf5-build/include" LDFLAGS="-L$DEPS_ROOT/hdf5-build/lib/" ./configure --prefix=$DEPS_ROOT/netcdf-build
