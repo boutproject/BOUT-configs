d1fb31111b0b8e6714ba21ac289be2c303458629
Giorgis Georgakoudis <ggeorgakoudis@gmail.com>
2024-02-01 08:42:31 -0800

    Add spack setup for perlmutter, update READMEs (#13)

---
+++ perlmutter/setup-env.sh
@@ -0,0 +1,11 @@
+echo "Setting up spack env on $(hostname)"
+# Disable MPICH GPU to avoid runtime error
+# TODO: fix
+export MPICH_GPU_SUPPORT_ENABLED=0
+
+# Import spack and setup packages
+export SPACK_USER_CONFIG_PATH=${PWD}/.spack
+export SPACK_USER_CACHE_PATH=${PWD}.spack
+. ${PWD}/../spack/share/spack/setup-env.sh
+spack env activate -p boutdev-spack-env
+echo $(spack install)
