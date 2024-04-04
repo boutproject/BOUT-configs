39a190632fa1a52c664969afb44a734bfa98b020
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2024-04-04 05:12:23 -0700

    Fix typo in script

--- perlmutter/setup-env.sh
+++ perlmutter/setup-env.sh
@@ -5,7 +5,7 @@ export MPICH_GPU_SUPPORT_ENABLED=0
 
 # Import spack and setup packages
 export SPACK_USER_CONFIG_PATH=${PWD}/.spack
-export SPACK_USER_CACHE_PATH=${PWD}.spack
+export SPACK_USER_CACHE_PATH=${PWD}/.spack
 . ${PWD}/../spack/share/spack/setup-env.sh
 spack env activate -p boutdev-spack-env
 echo $(spack install)
