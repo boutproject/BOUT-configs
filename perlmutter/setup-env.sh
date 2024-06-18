e35212a53da69b5aad6990b850acd7a380591d5c
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2024-06-18 11:29:19 -0700

    Fix MPI building on perlmutter

--- perlmutter/setup-env.sh
+++ perlmutter/setup-env.sh
@@ -1,11 +1,22 @@
-echo "Setting up spack env on $(hostname)"
-# Disable MPICH GPU to avoid runtime error
-# TODO: fix
-export MPICH_GPU_SUPPORT_ENABLED=0
+echo "Setting up BOUT++ spack env on $(hostname)..."
+module purge 2> /dev/null
+. /opt/cray/pe/cpe/23.12/restore_lmod_system_defaults.sh
+module load PrgEnv-gnu
+module load cmake/3.24.3
+module load gcc-native/12.3
+module load craype-x86-milan
+module load libfabric
+module load cudatoolkit/12.2
+
+# Set the path to the parent directory.
+BOUT_CONFIG_PATH=$(dirname "${BASH_SOURCE[0]:-$0}")
+echo "BOUT_CONFIG_PATH = ${BOUT_CONFIG_PATH}"
 
 # Import spack and setup packages
-export SPACK_USER_CONFIG_PATH=${PWD}/.spack
-export SPACK_USER_CACHE_PATH=${PWD}/.spack
-. ${PWD}/../spack/share/spack/setup-env.sh
-spack env activate -p boutdev-spack-env
-echo $(spack install --fail-fast)
+export SPACK_USER_CONFIG_PATH=${BOUT_CONFIG_PATH}/.spack
+export SPACK_USER_CACHE_PATH=${BOUT_CONFIG_PATH}/.spack
+. ${BOUT_CONFIG_PATH}/../spack/share/spack/setup-env.sh
+spack env activate -p ${BOUT_CONFIG_PATH}/boutdev-spack-env
+set -x
+spack install --fail-fast
+set +x
